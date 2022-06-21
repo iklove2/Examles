cc1cf587ab19e4d4c26e4660d34ed6d2928bb972
iklove2 <iklove@yandex.ru>
2022-06-21 22:46:45 +0300

    Ex003 Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberA / numberB;
+Console.Write("Частное: ");
+Console.WriteLine(result);
