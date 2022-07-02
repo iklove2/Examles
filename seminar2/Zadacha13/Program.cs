5706c6b3a8a33f969ab90e947d540c36eca71bfc
iklove2 <iklove@yandex.ru>
2022-07-02 23:50:15 +0300

    add task 13

---
+++ seminar2/Zadacha13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Ввведите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int delitel = 1;
+if (Convert.ToString(number).Length < 3)
+{
+    Console.WriteLine($"Для числа {number} третьей цифры нет");
+}
+else
+{
+    int digitn = Convert.ToString(number).Length;
+    for (int i = 0 ; i <= digitn - 3; i++)
+    {
+        delitel = delitel * 10;
+    }
+    Console.Write(delitel);
+    int number3 = (number % delitel) / (delitel/10);
+    Console.WriteLine($"Для числа {number} третья цифра {number3}");
+}
