8388e3613398b4e8c2ba741c98c657567bfe87c2
iklove2 <iklove@yandex.ru>
2022-07-02 23:16:35 +0300

    add task 10

---
+++ seminar2/Zadacha10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввведите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (Convert.ToString(number).Length != 3)
+{
+    Console.WriteLine("Число не трехзначное");
+}
+else
+{
+    Console.WriteLine(number);
+    int number2 = (number % 100) / 10;
+    Console.WriteLine($"Для числа {number} вторая цифра {number2}");
+}
