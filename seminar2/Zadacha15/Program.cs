2a61127f6ccebf7e1a3e378ee786caa3b5c8cc53
iklove2 <iklove@yandex.ru>
2022-07-03 00:04:56 +0300

    add task 13

---
+++ seminar2/Zadacha15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Узнаем статус дня (выходной не выходной)");
+Console.Write("Введите порядковый номер дня недели от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber == 6)
+{
+    Console.WriteLine($"День недели с номером {dayNumber} выходной день и это суббота");
+}
+else if (dayNumber == 7)
+{
+    Console.WriteLine($"День недели с номером {dayNumber} выходной день и это воскресенье");
+}
+else if (dayNumber >= 1 && dayNumber < 6)
+{
+    Console.WriteLine($"День недели с номером {dayNumber} не выходной");
+}
+else if (dayNumber < 1 || dayNumber > 7)
+{
+    Console.WriteLine($"Дня недели с номером {dayNumber} нет");
+}
